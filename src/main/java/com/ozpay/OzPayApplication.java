1e9793c1a1bfb92fe6a61ccc7e5ce01414690127
vitinh0z <victor10.vg23@gmail.com>
2026-01-13 00:34:32 -0300

    initial commit

---
+++ src/main/java/com/ozpay/OzPayApplication.java
@@ -0,0 +1,13 @@
+package com.ozpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozpay/OzPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
